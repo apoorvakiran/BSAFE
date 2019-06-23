252cada9253687c945987d1ba6bf4caf674c745d
Jesper Kristensen <jespertoftkristensen@gmail.com>
2019-06-23 14:47:01 -0400

    The buket interactions (download + upload) now is confirmed to work with Iterate Labs' account.

--- Data/load_aws_s3.py
+++ Data/load_aws_s3.py
@@ -3,10 +3,43 @@
 This file is to demonstrate how to download and upload data
 from the AWS S3 buckets.
 
-The data goes from the glove to AWS to SAFE!
+To run open a terminal and type:
+    >> python test_aws.py
+
+============================================================
+Here are instructions on how to create and download the
+correct credentials to access the S3 bucket:
+============================================================
+First, you need to make sure your credentials are correct. These credentials are
+like your own set of keys to access the bucket. So each member need their own set of keys.
+
+This is done by creating a folder ".aws" in your home directory. Open your terminal and
+type this command:
+    >> mkdir ~/.aws
+
+Then also create the file "credentials" (note: no extension needed):
+    >> vim ~/.aws/credentials
+
+This opens up VI (use whatever other editor you want - I like VIM).
+In the file, please put the following three lines:
+[default]
+aws_access_key_id = <your access key - get this from AWS>
+aws_secret_access_key = <your secret key - get this from AWS>
+
+Note the stuff inside the "<>" brackets: These are keys you need to obtain from AWS and they are
+personal to you. To get them sign in to your AWS console then go to the top right under "security credentials".
+Look under "Access keys for CLI, SDK, & API access" where you will be able to create and download your keys.
+
+So an example of the three lines is:
+[default]
+aws_access_key_id = ga3kadlgjrhap495
+aws_secret_access_key = dWaFiwaiawFgpa391295D8
+
+In conclusion, this script shows us how the data goes from the glove to AWS and now to SAFE!
+Questions? Contact Jesper Kristensen or James Russo.
 
 @ author Jesper Kristensen
-Copyright 2018-
+Copyright IterateLabs.co 2018-
 """
 
 __all__ = ["upload_data_to_aws_s3", "download_from_aws_s3"]
@@ -15,6 +48,7 @@ __version__ = "Alpha"
 
 import boto3
 import botocore
+from boto3.exceptions import S3UploadFailedError
 
 
 def upload_data_to_aws_s3(bucketname=None, filename=None):
@@ -28,7 +62,14 @@ def upload_data_to_aws_s3(bucketname=None, filename=None):
     """
     # Create an S3 client
     s3 = boto3.client('s3')
-    s3.upload_file(filename, bucketname, filename)
+
+    try:
+        s3.upload_file(filename, bucketname, filename)
+    except S3UploadFailedError as e:
+        # something did not work!
+        print("There was an error uploading the file to the S3 bucket!")
+        print("The error is: {}".format(e))
+        print("Did you update your local ~/.aws/credentials file?")
 
 
 def download_from_aws_s3(bucketname=None, filename=None,
@@ -45,4 +86,8 @@ def download_from_aws_s3(bucketname=None, filename=None,
         if e.response['Error']['Code'] == "404":
             print("The object does not exist in the AWS S3 bucket.")
         else:
+            # something did not work!
+            print("There was an error downloading the file to the S3 bucket!")
+            print("The error is: {}".format(e))
+            print("Did you update your local ~/.aws/credentials file?")
             raise
--- Test/test_aws.py
+++ Test/test_aws.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Testing the AWS S3 interaction.
+Testing the Iterate Labs Co AWS S3 interaction with SAFE.
+Doing things locally for now, but next step is to put SAFE on AWS as
+well in a kind of EC2 instance and have everything work in the cloud!
 
 The examples on AWS usage are here:
 https://docs.aws.amazon.com/code-samples/latest/catalog/python-s3-s3-python-example-upload-file.py.html
 
 @ author Jesper Kristensen
-Copyright 2019
+Copyright Iterate Labs, Co. 2018-
 """
 
 __author__ = "Jesper Kristensen"
@@ -15,17 +17,20 @@ __version__ = "Alpha"
 import os
 from Data import upload_data_to_aws_s3, download_from_aws_s3
 
+# put the bucket name here:
+BUCKET_NAME = 'testbucketjesperkristensen'
+# FYI this is a test bucket that I, Jesper, created under Iterate Lab's S3 account.
+
 
 def test_upload():
     # Let's upload a file to the S3 bucket
     # create a test file (any file for now):
     os.system("touch data_from_glove_example.txt")
 
-    upload_data_to_aws_s3(bucketname='thisisacoolbucket1234',
+    upload_data_to_aws_s3(bucketname=BUCKET_NAME,
                           filename='data_from_glove_example.txt')
 
     print("Data has been uploaded!")
-
     # now remove the file locally:
     os.remove("data_from_glove_example.txt")
 
@@ -38,7 +43,7 @@ def test_download():
     if os.path.isfile(local_filename):
         os.remove(local_filename)
 
-    download_from_aws_s3(bucketname='thisisacoolbucket1234',
+    download_from_aws_s3(bucketname=BUCKET_NAME,
                          filename='data_from_glove_example.txt',
                          local_filename=local_filename)
 
@@ -46,5 +51,4 @@ def test_download():
     os.remove(local_filename)  # remove the downloaded file
 
     print("Data was downloaded correctly!")
-
     # at this point we can download the data from the bucket!
