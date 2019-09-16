03e297315537d0ff70542447d0578fe38b68c5e6
jrusso1020 <james.russo@iteratelabs.co>
2019-09-16 17:27:15 -0400

    Add sentry
    Add sentry so that we can track errors that happen on staging and
    production.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from periodiq import PeriodiqMiddleware
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from .extensions import dramatiq
 from .api import api
 
 def create_app():
+    sentry_sdk.init(
+        dsn="https://4ca7cdcc54274295af09b1f2d98f4960@sentry.io/1728777",
+        integrations=[FlaskIntegration()]
+    )
     app = Flask(__name__)
 
     # Beware that app config must be loaded before you initialize Dramatiq
