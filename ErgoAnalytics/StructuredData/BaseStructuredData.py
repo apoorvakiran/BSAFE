c0e343d597a55cb04d5b1f5ec025bc05926fc092
jrusso1020 <james.russo@iteratelabs.co>
2019-09-16 17:24:42 -0400

    Query data from aws elasticsearch
    With this change we will be able to query data from aws elasticsearch.
    This will allow us to run our analysis on AWS on demand using real data.
    Something to note that there is a known bug with python and urllib
    that causes this to error out on macOSX, in order to get around this
    you need to set the environment variable `NO_PROXY=*` in order to run
    the dramatiq worker without causing a segmentation fault.

--- ErgoAnalytics/StructuredData/BaseStructuredData.py
+++ ErgoAnalytics/StructuredData/BaseStructuredData.py
@@ -10,11 +10,13 @@ __all__ = ["BaseStructuredData"]
 __author__ = "Jesper Kristensen"
 __version__ = "Alpha"
 
+import logging
 import numpy as np
 import pandas as pd
 from .. import is_numeric
 from Settings import FRACTION_OF_DATA_USEFUL
 
+logger = logging.getLogger()
 
 class BaseStructuredData(object):
     """
@@ -48,15 +50,15 @@ class BaseStructuredData(object):
         :return:
         """
 
-        print(f"# of raw data points = {len(data)}")
+        logger.info(f"# of raw data points = {len(data)}")
 
         if len(data) <= 1:
             raise Exception("There is <= 1 data point(s) in the file!")
 
-        print("Checking that dates make sense...")
+        logger.info("Checking that dates make sense...")
 
         # demand that at least 70% of the data is useful
-        print(f"Demanding that at least {FRACTION_OF_DATA_USEFUL * 100}% of "
+        logger.info(f"Demanding that at least {FRACTION_OF_DATA_USEFUL * 100}% of "
               f"the data is useful!")
         last_index = int(data.shape[0] * FRACTION_OF_DATA_USEFUL)
 
@@ -69,12 +71,12 @@ class BaseStructuredData(object):
                                 "check the data format")
         except ValueError as ve:
             # we need
-            print("Got value error in trying to convert date-time:")
-            print(ve)
-            print("Please manually change this to the correct format!")
+            logger.info("Got value error in trying to convert date-time:")
+            logger.info(ve)
+            logger.info("Please manually change this to the correct format!")
             raise
 
-        print("loop to find if there are date-issues regarding back-and-forth "
+        logger.info("loop to find if there are date-issues regarding back-and-forth "
               "in time, use std dev")
 
         data = BaseStructuredData._sigma_filter(data, last_index=last_index)
@@ -89,36 +91,36 @@ class BaseStructuredData(object):
         try:
             # shape of data before filtering the specific names
             # we are asking for:
-            print("  > Before getting the relevant columns the data has # of "
+            logger.info("  > Before getting the relevant columns the data has # of "
                   "columns = {} (len names = {})".format(data.shape[1],
                                                          len(names)))
             data = data[names]  # just get the names above
-            print("  > After getting the relevant columns the data has # of "
+            logger.info("  > After getting the relevant columns the data has # of "
                   "columns = {}".format(data.shape[1]))
         except KeyError:
-            print("Looks like the delta values may not be in the data? "
+            logger.info("Looks like the delta values may not be in the data? "
                   "Delta yaw etc.? So skipping the last 3 columns")
             # delta values may not be in the index, so ignore those:
             data = data[names[:-3]]
 
         # now we have the filtered data;
         # make sure to sort it by date:
-        print("Sorting the data by time...")
+        logger.info("Sorting the data by time...")
 
         data.sort_values(by=['Date-Time'], ascending=True, inplace=True)
 
-        print("Dropping duplicates...")
-        print("    # rows before = {}".format(len(data)))
+        logger.info("Dropping duplicates...")
+        logger.info("    # rows before = {}".format(len(data)))
         data.drop_duplicates(subset=['Date-Time'], inplace=True)
-        print("    # rows after = {}".format(len(data)))
+        logger.info("    # rows after = {}".format(len(data)))
         # we should reset the index too
-        print("Resetting the index too")
+        logger.info("Resetting the index too")
         data = data.reset_index(drop=True)
-        print(f"Summary: # of data points after basic pre-processing: "
+        logger.info(f"Summary: # of data points after basic pre-processing: "
               f"{len(data)}")
 
         if not is_streaming and len(data) <= 1:
-            print("The data was in a bad quality - we only have 1 data point "
+            logger.info("The data was in a bad quality - we only have 1 data point "
                   "left after some basic pre-processing!")
             raise Exception("Bad quality data")
 
@@ -179,11 +181,11 @@ class BaseStructuredData(object):
         if (not is_streaming) and \
                 (not (pd.to_datetime(data['Date-Time']).iloc[1] -
                       pd.to_datetime(data['Date-Time']).iloc[0]).seconds <= 1):
-            print("The data seems to have a difference in time of 1 "
+            logger.info("The data seems to have a difference in time of 1 "
                   "second or more?")
-            print("The frequency is expected to be closer to 1/100th of a "
+            logger.info("The frequency is expected to be closer to 1/100th of a "
                   "second!")
-            print("Please double-check the date-times.")
+            logger.info("Please double-check the date-times.")
             raise Exception("Date-time frequency error in collection!")
 
     def _data_imputation_filter(self, data=None, method='nan_filter'):
@@ -201,10 +203,10 @@ class BaseStructuredData(object):
         # and then sample from it conditioned on the data we _do_ have at other
         # rows to find likely values for the missing data.
         if method == 'nan_filter':
-            print("> # of data before filtering for NaNs... = "
+            logger.info("> # of data before filtering for NaNs... = "
                   "{}".format(len(data)))
             data.dropna(how='any', inplace=True)
-            print("> # of data after filtering away NaNs... = "
+            logger.info("> # of data after filtering away NaNs... = "
                   "{}".format(len(data)))
         else:
             raise NotImplementedError("Implement me!")
