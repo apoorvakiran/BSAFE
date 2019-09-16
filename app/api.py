b34b1329c9422c385a2de2f138f3720edb9e55e5
jrusso1020 <james.russo@iteratelabs.co>
2019-09-16 11:23:04 -0400

    Integrate analytics with flask application
    With this change we will integrate our analytics code with our flask
    web application. This will allow us to run our analysis on demand
    as well as automatically on AWS using our flask application. This
    also changes our logging slightly to give us more info. With this change
    you should be able to run the flask application and issue the curl command
    `curl "localhost:5000/api/safety_score?mac=F6:12:3D:BD:DE:44&from_date=2019-03-01T00:00:00-05:00&till_date=2019-04-01T00:00:00-05:00"`
    to issue a request to run the analysis for a given wearable
    for a given time period.

--- app/api.py
+++ app/api.py
@@ -1,16 +1,21 @@
+import logging
 from flask import Blueprint, request, jsonify
 
-from .tasks import process_job
+from .tasks import safety_score_analysis
 
 api = Blueprint('api', 'api', url_prefix='/api')
+
+logger = logging.getLogger()
 
 @api.route('/safety_score', methods=['GET'])
 def generate_safety_score():
     mac = request.args.get('mac', default = None, type = str)
-    if mac is None:
-        return jsonify({'error': 'Mac address is required'}), 422
-    print('received')
-    process_job.send(mac)
+    from_date = request.args.get('from_date', default = None, type = str)
+    till_date = request.args.get('till_date', default = None, type = str)
+    if mac is None or from_date is None or till_date is None:
+        return jsonify({'error': 'Missing required parameter'}), 422
+    logger.info("Received mac #%s.", mac)
+    safety_score_analysis.send(mac, from_date, till_date)
     return jsonify({'status': 'processed'}), 200
 
 
