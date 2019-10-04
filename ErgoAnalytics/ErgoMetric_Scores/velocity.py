9ca95085950f3ff0b8aca34d4c214f3f80d4a031
jesperkristensen58 <jesper.kristensen@iteratelabs.co>
2019-10-04 19:05:20 -0400

    code refactoring and updated ergometrics test

--- ErgoAnalytics/ErgoMetric_Scores/velocity.py
+++ ErgoAnalytics/ErgoMetric_Scores/velocity.py
@@ -11,6 +11,7 @@ __author__ = "Jesper Kristensen"
 __version__ = "Alpha"
 
 from numpy import gradient
+from numpy import absolute
 from numpy import std
 import logging
 
@@ -20,24 +21,23 @@ logger = logging.getLogger()
 def compute_velocity_score(delta_pitch=None, delta_yaw=None, delta_roll=None):
     """
     Computes the velocity score.
-    """
-    yawgradient = gradient(delta_yaw)
-    pitchgradient = gradient(delta_pitch)
-    rollgradient = gradient(delta_roll)
 
-    n = 0
-    yaw = []
-    pitch = []
-    roll = []
-    while n < len(yawgradient):
-
-        if yawgradient[n] < 100:
-            yaw.append(yawgradient[n])
-        if pitchgradient[n] < 100:
-            pitch.append(pitchgradient[n])
-        if rollgradient[n] < 100:
-            roll.append(rollgradient[n])
-
-        n += 1
+    First, velocity is approximated as the gradient of the angle in time. We imagine
+    this creating a distribution of instantenous values. When this distribution is
+    wide it means that we see many high-valued velocities.
+    So the wider / higher std dev the higher the score should be.
     
-    return std(pitch) * 7, std(yaw) * 7, std(roll) * 7
+    This measures the width of the distribution of instantenous velocities.
+    """
+    gradient_yaw = gradient(delta_yaw)
+    gradient_pitch = gradient(delta_pitch)
+    gradient_roll = gradient(delta_roll)
+    
+    THRESHOLD_GRAD = 100
+    #
+    std_yaw = gradient_yaw[absolute(gradient_yaw) < THRESHOLD_GRAD].std()
+    std_pitch = gradient_pitch[absolute(gradient_pitch) < THRESHOLD_GRAD].std()
+    std_roll = gradient_roll[absolute(gradient_roll) < THRESHOLD_GRAD].std()
+    
+    # TODO(Jesper): Can also use np.percentile here - curious what would be the diff
+    return std_pitch * 7, std_yaw * 7, std_roll * 7
