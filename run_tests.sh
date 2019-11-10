8350a387e0e026feb6c9b82604fddf258dfad46a
jesperkristensen58 <jesper.kristensen@iteratelabs.co>
2019-11-10 15:23:41 -0500

    ignoring system tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 # Copyright Iterate Labs, Inc.
 
 # Run tests of the BSAFE code base in parallel over 4 workers:
-pipenv run python -m pytest -v -n 4 tests/
+pipenv run python -m pytest -v -n 4 --ignore tests/system tests tests/
 
