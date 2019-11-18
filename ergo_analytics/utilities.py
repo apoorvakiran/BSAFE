545b0b61d52047fc77bffc69ff0115050e93a3d9
jesperkristensen58 <jesper.kristensen@iteratelabs.co>
2019-11-17 19:36:07 -0500

    subsampling implemented

--- ergo_analytics/utilities.py
+++ ergo_analytics/utilities.py
@@ -6,14 +6,91 @@ Contains utilities for ErgoAnalytics.
 Copyright 2018 Iterate Labs, Inc.
 """
 
-__all__ = ["is_numeric", "digitize_values", "rad_to_deg"]
+__all__ = ["is_numeric", "digitize_values", "rad_to_deg", "subsample_data"]
 __author__ = "Jesper Kristensen"
 __copyright__ = "Copyright (C) 2018- Iterate Labs, Inc."
 __version__ = "Alpha"
 
+import logging
 from numpy import abs
 from numpy import digitize
 from numpy import degrees
+from numpy import array_split
+from numpy import arange
+from numpy import median
+from numpy import where
+from numpy.random import choice
+
+logger = logging.getLogger()
+
+
+def subsample_data(data=None, number_of_subsamples=100,
+                   use_subsampling=True,
+                   subsample_size_index=1000, randomize=True,
+                   **kwargs):
+    """
+    This is a utility which can create chunks of data in various useful
+    formats including support for randomization.
+
+    Given incoming "data" as a dataframe, can break that
+    data into "number_of_subsamples" subsamples each of
+    size "subsample_size_index".
+
+    This is an iterator which will return the chunks of data subsamples.
+    """
+
+    if data is None:
+        yield None, dict()
+        return
+
+    if subsample_size_index == 0 or number_of_subsamples == 0:
+        yield [], dict()
+        return
+
+    if not use_subsampling or data is None:
+        chunk_info = dict()
+        yield data, chunk_info
+        return
+
+
+    if not randomize:
+
+        # generate all chunks:
+        num_chunks_target = max(len(data) // subsample_size_index, 1)
+        all_chunks = array_split(data, num_chunks_target)
+        num_chunks = len(all_chunks)
+
+        logger.debug(
+            f"Splitting data into {num_chunks} chunks! "
+            f"(requested: {num_chunks_target})")
+
+        # at this point, we have less chunks requested than we split the
+        # data into, so now we need to evenly return a set of "num_chunks"
+        # chunks. We can do this by leveraging KFold from scikit-learn
+        # but just operate on indices:
+        chunk_indices = arange(num_chunks)
+
+        for ixs in array_split(chunk_indices, number_of_subsamples):
+            if len(ixs) == 0:
+                return
+            ix = int(median(ixs))
+            yield all_chunks[ix], dict()
+
+    else:
+        # we do randomize the chunks returned:
+
+        # so just keep generating index sets:
+        # draw a random number in the range [0, len(data) - sample_size]:
+
+        valid_start_indices = arange(len(data))
+        valid_start_indices = valid_start_indices[:where(valid_start_indices ==
+                                    len(data) - subsample_size_index)[0][0]]
+
+        random_start_indices = choice(valid_start_indices,
+                                      size=number_of_subsamples, replace=False)
+
+        for ix in random_start_indices:
+            yield data.iloc[ix:ix + subsample_size_index], dict()
 
 
 def is_numeric(val):
--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 # * Copyright Iterate Labs, Inc. *
 # ********************************
 # Run tests and linters over code
+echo
 
 # Run tests of the BSAFE code base in parallel over 4 workers:
 echo "Run pytest to unit, integration, and system tests..."
@@ -9,7 +10,12 @@ pipenv run python -m pytest -v -n 4 --ignore tests/system tests tests/
 echo "[completed]"
 
 # Run pylama as a linter / code checker - make sure our code looks good:
-echo "Make sure code quality is OK."
+echo
+echo "Now making sure code quality is OK."
 pipenv run python -m pytest --pylama -n 4 ergo_analytics/
 echo "[completed]"
+
+echo
+echo "[ALL OK] All good to push commit!"
+echo
 
