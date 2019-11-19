b2006ce622201652b27c66da990c0dfe4c7095d2
jesperkristensen58 <jesper.kristensen@iteratelabs.co>
2019-11-18 23:30:05 -0500

    ergo test needs rewriting

--- tests/ergo_analytics/_ergo_metrics.py
+++ tests/ergo_analytics/_ergo_metrics.py
@@ -18,6 +18,16 @@ __author__ = "Jesper Kristensen"
 __copyright__ = "Copyright (C) 2018- Iterate Labs, Inc."
 __version__ = "Alpha"
 
+import os
+import sys
+
+# == we start by finding the project root:
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+while not os.path.split(ROOT_DIR)[1] == 'BSAFE':
+    ROOT_DIR = os.path.dirname(ROOT_DIR)  # cd ../
+sys.path.insert(0, ROOT_DIR)  # now insert into our Python path
+# ==
+
 import pytest
 from unittest.mock import MagicMock
 from ergo_analytics.ergo_metrics import ErgoMetrics
@@ -29,18 +39,20 @@ def test_total_score():
     """
 
     # pretend like this data in the "return_value" is what we collected
-    data = MagicMock()
-    data.get_data = MagicMock(return_value=[1,2,3,4,2,8] * 2)
+    data_chunks = MagicMock()
+    data_chunks.get_data = MagicMock(
+        return_value=[[1,2,3,4,2,8] * 2])
 
-    em = ErgoMetrics(structured_data=data)
+    em = ErgoMetrics(list_of_structured_data_chunks=data_chunks)
     em.compute()
 
     assert pytest.approx(em.get_score(name='total'), 0.0001) == 1.346153
     
-    data = MagicMock()
-    data.get_data = MagicMock(return_value=[20, 40, 60, 80, 80, 80] * 2)
+    data_chunks = MagicMock()
+    data_chunks.get_data = MagicMock(
+        return_value=[[20, 40, 60, 80, 80, 80] * 2])
     
-    em = ErgoMetrics(structured_data=data)
+    em = ErgoMetrics(list_of_structured_data_chunks=data_chunks)
     em.compute()
-    
+
     assert pytest.approx(em.get_score(name='total'), 0.0001) == 7
