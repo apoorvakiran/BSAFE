ac095bcde40bfc08051e95fe3b8e5b3fe2b802cc
jesperkristensen58 <jesper.kristensen@iteratelabs.co>
2019-12-22 19:07:03 -0500

    fix test

--- tests/ergo_analytics/data_raw/test_load_data_store.py
+++ tests/ergo_analytics/data_raw/test_load_data_store.py
@@ -41,9 +41,8 @@ def test_load_data_storage_basic():
     """
 
     ds = LoadDataStore()
-    raw_df = ds.load(tester='john', project='johnson')
-
-    assert len(raw_df) == 1750
+    for raw_df, _ in ds.load(tester='john', project='johnson', all=False):
+        assert len(raw_df) == 1750
 
 
 def test_compute_score():
@@ -53,33 +52,34 @@ def test_compute_score():
     :return:
     """
     ds = LoadDataStore()
-    raw_df = ds.load(tester='john', project='johnson')
 
-    raw_df.dropna(how='all', inplace=True)
+    for raw_df, _ in ds.load(tester='john', project='johnson', all=False):
 
-    # make some conversions:
-    raw_df.iloc[:, 0] = pd.to_datetime(raw_df.iloc[:, 0])
-    for j in range(1, 6 + 1):
-        raw_df.iloc[:, j] = raw_df.iloc[:, j].astype(float)
+        raw_df.dropna(how='all', inplace=True)
 
-    pipeline = DataFilterPipeline()
-    # instantiate the filters:
-    pipeline.add_filter(name='fix_osc', filter=FixDateOscillations())
-    pipeline.add_filter(name='centering1', filter=DataCentering())
-    pipeline.add_filter(name='delta_values', filter=ConstructDeltaValues())
-    pipeline.add_filter(name='centering2', filter=DataCentering())
-    pipeline.add_filter(name='zero_shift_filter', filter=ZeroShiftFilter())
-    pipeline.add_filter(name='window', filter=WindowOfRelevantDataFilter())
-    pipeline.add_filter(name='impute', filter=DataImputationFilter())
-    pipeline.add_filter(name='quadrant_fix', filter=QuadrantFilter())
-    # run the pipeline!
-    structured_data_chunks = pipeline.run(on_raw_data=raw_df,
-                                   with_format_code='5',
-                                   use_subsampling=False)
+        # make some conversions:
+        raw_df.iloc[:, 0] = pd.to_datetime(raw_df.iloc[:, 0])
+        for j in range(1, 6 + 1):
+            raw_df.iloc[:, j] = raw_df.iloc[:, j].astype(float)
 
-    metrics = ErgoMetrics(list_of_structured_data_chunks=structured_data_chunks)
-    metrics.compute()
-    score = metrics.get_score()
+        pipeline = DataFilterPipeline()
+        # instantiate the filters:
+        pipeline.add_filter(name='fix_osc', filter=FixDateOscillations())
+        pipeline.add_filter(name='centering1', filter=DataCentering())
+        pipeline.add_filter(name='delta_values', filter=ConstructDeltaValues())
+        pipeline.add_filter(name='centering2', filter=DataCentering())
+        pipeline.add_filter(name='zero_shift_filter', filter=ZeroShiftFilter())
+        pipeline.add_filter(name='window', filter=WindowOfRelevantDataFilter())
+        pipeline.add_filter(name='impute', filter=DataImputationFilter())
+        pipeline.add_filter(name='quadrant_fix', filter=QuadrantFilter())
+        # run the pipeline!
+        structured_data_chunks = pipeline.run(on_raw_data=raw_df,
+                                       with_format_code='5',
+                                       use_subsampling=False)
 
-    assert score is not None
-    assert score >= 0 and score <= 7
+        metrics = ErgoMetrics(list_of_structured_data_chunks=structured_data_chunks)
+        metrics.compute()
+        score = metrics.get_score()
+
+        assert score is not None
+        assert score >= 0 and score <= 7
