b1eb82057d42d541708c740deb989b1214025e8f
Jesper Kristensen <jespertoftkristensen@gmail.com>
2020-06-25 12:29:32 -0400

    [noticket] formatting fix.

--- app/tasks.py
+++ app/tasks.py
@@ -37,19 +37,20 @@ from .extensions import dramatiq
 logger = logging.getLogger()
 api_client = ApiClient()
 
-@dramatiq.actor(periodic=cron('*/15 * * * *'))
+
+@dramatiq.actor(periodic=cron("*/15 * * * *"))
 def automated_analysis():
     logger.info("Running automated analysis")
     current_time = datetime.datetime.utcnow()
     end_time = datetime.datetime.utcnow().isoformat()
     start_time = (current_time - datetime.timedelta(minutes=15)).isoformat()
     try:
-        response = api_client.get_request('api/v1/wearables?automated=true')
+        response = api_client.get_request("api/v1/wearables?automated=true")
         response.raise_for_status()
-        wearables = response.json()['data']
+        wearables = response.json()["data"]
         logger.info(f"Running automated analysis for {len(wearables)}")
         for wearable in wearables:
-            mac_address = wearable['attributes']['mac']
+            mac_address = wearable["attributes"]["mac"]
             logger.info(f"Running analysis for {mac_address}")
             safety_score_analysis.send(mac_address, start_time, end_time)
         logger.info(f"Enqueued all analysis")
@@ -62,29 +63,33 @@ def automated_analysis():
 @dramatiq.actor(max_retries=3)
 def safety_score_analysis(mac_address, start_time, end_time):
     logger.info(f"Getting safety score for {mac_address}")
-    index = os.getenv('ELASTIC_SEARCH_INDEX', 'iterate-labs-local-poc')
-    host = os.getenv('ELASTIC_SEARCH_HOST')
+    index = os.getenv("ELASTIC_SEARCH_INDEX", "iterate-labs-local-poc")
+    host = os.getenv("ELASTIC_SEARCH_HOST")
 
-    data_format_code = '5'  # what format is the data in?
+    data_format_code = "5"  # what format is the data in?
 
     # subsampling of the data:
-    how_to_combine_across_parameter = 'average'  # note: cannot be "keep separate"
+    how_to_combine_across_parameter = "average"  # note: cannot be "keep separate"
     number_of_subsamples = 10
     randomize_subsampling = False
     use_subsampling = False
 
     data_loader = LoadElasticSearch()
-    raw_data = data_loader.retrieve_data(mac_address=mac_address,
-                                         start_time=start_time,
-                                         end_time=end_time,
-                                         host=host, index=index,
-                                         data_format_code=data_format_code)
+    raw_data = data_loader.retrieve_data(
+        mac_address=mac_address,
+        start_time=start_time,
+        end_time=end_time,
+        host=host,
+        index=index,
+        data_format_code=data_format_code,
+    )
 
     if raw_data is None:
         logger.info(f"Found no elements in the ES database for {mac_address}.")
         return
-    logger.info(f"Found {len(raw_data)} elements in "
-                f"the ES database for {mac_address}.")
+    logger.info(
+        f"Found {len(raw_data)} elements in " f"the ES database for {mac_address}."
+    )
     logger.info(raw_data.head(10))
 
     num_data = len(raw_data)
@@ -95,35 +100,35 @@ def safety_score_analysis(mac_address, start_time, end_time):
     # now pass the raw data through our data filter pipeline:
     pipeline = DataFilterPipeline()
     # instantiate the filters:
-    pipeline.add_filter(name='fix_osc', filter=FixDateOscillations())
+    pipeline.add_filter(name="fix_osc", filter=FixDateOscillations())
     # pipeline.add_filter(name='centering1', filter=DataCentering())
-    pipeline.add_filter(name='delta_values', filter=ConstructDeltaValues())
-    pipeline.add_filter(name='centering2', filter=DataCentering())
-    pipeline.add_filter(name='window', filter=WindowOfRelevantDataFilter())
-    pipeline.add_filter(name='impute', filter=DataImputationFilter())
-    pipeline.add_filter(name='quadrant_fix', filter=QuadrantFilter())
+    pipeline.add_filter(name="delta_values", filter=ConstructDeltaValues())
+    pipeline.add_filter(name="centering2", filter=DataCentering())
+    pipeline.add_filter(name="window", filter=WindowOfRelevantDataFilter())
+    pipeline.add_filter(name="impute", filter=DataImputationFilter())
+    pipeline.add_filter(name="quadrant_fix", filter=QuadrantFilter())
     # run the pipeline!
 
     raw_data.to_csv("log.csv", index=False)
     print("Stored!")
 
-    list_of_structured_data_chunks = pipeline.run(on_raw_data=raw_data,
-                                            with_format_code=data_format_code,
-                                            use_subsampling=use_subsampling,
-                                    randomize_subsampling=randomize_subsampling,
-                                    subsample_size_index=subsample_size_index,
-                                    number_of_subsamples=number_of_subsamples)
+    list_of_structured_data_chunks = pipeline.run(
+        on_raw_data=raw_data,
+        with_format_code=data_format_code,
+        use_subsampling=use_subsampling,
+        randomize_subsampling=randomize_subsampling,
+        subsample_size_index=subsample_size_index,
+        number_of_subsamples=number_of_subsamples,
+    )
 
     logger.info(f"Retrieved all data for {mac_address}")
     if len(list_of_structured_data_chunks) > 0:
         logger.info(f"Has data to run analysis on for {mac_address}")
-        em = ErgoMetrics(
-            list_of_structured_data_chunks=list_of_structured_data_chunks)
+        em = ErgoMetrics(list_of_structured_data_chunks=list_of_structured_data_chunks)
         # add metrics to compute:
-        metrics = {"activity": AngularActivityScore,
-                   "posture": PostureScore}
+        metrics = {"activity": AngularActivityScore, "posture": PostureScore}
         for m_name in metrics:
-            em.add(metric=metrics[m_name], name=m_name)
+            em.add(metric=metrics[m_name])
         em.compute()
         logger.info(f"Metrics generated for {mac_address}")
         # the report is set up in the context of a device and its
@@ -138,8 +143,7 @@ def safety_score_analysis(mac_address, start_time, end_time):
             mac_address=mac_address,
         )
         logger.info(report.response)
-        logger.info(f"{report.response.status_code} "
-                    f"{report.response.text}")
+        logger.info(f"{report.response.status_code} " f"{report.response.text}")
         logger.info(f"Created safety_score for {mac_address}")
 
         logger.info("JSON = {}".format(report.to_json()))
