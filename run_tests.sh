4a40abe6c6622741b45dbe3c1dc42e71bff6adc0
Jessie <jessie.zhangci@gmail.com>
2020-08-26 11:14:43 -0400

    modified weighted avg

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ echo
 
 # Run tests of the Data Store code base in parallel over 4 workers:
 echo "Run pytest to unit, integration, and system tests..."
-pipenv run python -m pytest -nauto tests/ --pdb
+pipenv run python -m pytest tests/ --pdb
 # echo "[completed]"
 
 # Run pylama as a linter / code checker - make sure our code looks good:
