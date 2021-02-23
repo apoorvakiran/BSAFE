141b388be6a53be40ff8b7d81104c5cfd6a8515f
Jessie <jessie.zhangci@gmail.com>
2021-02-23 10:58:55 -0800

    Initialize PeakAnalysis

---
+++ productivity/peak_analysis.py
@@ -0,0 +1,8 @@
+"""
+Compute several metrics on peak and cycle analysis. This gives us information on
+motion frequency.
+
+@ author: Jessie Zhang
+Copyright Iterate Labs 2018-
+All Rights Reserved.
+"""
